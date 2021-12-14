eb1923d963bfedd366ac2600f120fd4523f80dc4
佐々木勇輔 <ys.sasaki@uchida-it.co.jp>
2021-12-14 13:10:53 +0900

    テストコード追加

--- TddLibrary/TddLibrary/FizzBuzz.cs
+++ TddLibrary/TddLibrary/FizzBuzz.cs
@@ -4,15 +4,20 @@
     {
         public string convertTest(int num)
         {
+            if (num % 3 == 0 && num % 5 == 0)
+            {
+                return "FizzBuzz";
+            }
             if (num % 3 == 0)
             {
                 return "Fizz";
                 
             }
-            if(num % 5 == 0)
+            if (num % 5 == 0)
             {
                 return "Buzz";
             }
+
             return num.ToString();
         }
     }
--- TddLibrary/TddLibraryTest/UnitTest1.cs
+++ TddLibrary/TddLibraryTest/UnitTest1.cs
@@ -16,7 +16,7 @@ namespace TddLibraryTest
         }
 
 
-        class _3の倍数の時は数の代わりにFizzを変換する
+        class _3の倍数の時は数の代わりにFizzに変換する
         {
             private FizzBuzz fizzbuzz;
 
@@ -28,14 +28,29 @@ namespace TddLibraryTest
             }
 
 
+            //[Test]
+            //public void 同値類の中の最小の3の倍数である3を渡すと文字列Fizzを返す()
+            //{
+            //    Assert.AreEqual("Fizz", fizzbuzz.convertTest(3));
+            //}
+            //[Test]
+            //public void 上限の境界値の一つ内側であり同値類の中の最大の3の倍数である99を渡すと文字列Fizzを返す()
+            //{
+            //    Assert.AreEqual("Fizz", fizzbuzz.convertTest(99));
+            //}
             [Test]
-            public void _3を渡すと文字列Fizzを返す()
+            [TestCase(3, TestName = " 同値類の中の最小の3の倍数である3を渡すと文字列Fizzを返す")]
+            [TestCase(99, TestName = " 上限の境界値の一つ内側であり同値類の中の最大の3の倍数である99を渡すと文字列Fizzを返す")]
+            public void _3の倍数の時は数の代わりにFizzに変換するよ(int num)
             {
-                Assert.AreEqual("Fizz", fizzbuzz.convertTest(3));
+                Assert.AreEqual("Fizz", fizzbuzz.convertTest(num));
             }
+
         }
 
-        class _5の倍数の時は数の代わりにBuzzを変換する
+        
+
+        class _5の倍数の時は数の代わりにBuzzに変換する
         {
             private FizzBuzz fizzbuzz;
 
@@ -48,13 +63,37 @@ namespace TddLibraryTest
 
 
             [Test]
-            public void _5を渡すと文字列Buzzを返す()
+            public void 同値類の中の最小の5の倍数である5を渡すと文字列Buzzを返す()
             {
                 Assert.AreEqual("Buzz", fizzbuzz.convertTest(5));
             }
+            [Test]
+            public void 上限の境界値であり同値類の中の最大の5の倍数である100を渡すと文字列Buzzを返す()
+            {
+                Assert.AreEqual("Buzz", fizzbuzz.convertTest(100));
+            }
         }
 
-        class _その他の数の時はそのまま文字列に変換する
+        class _3と5両方の倍数の時は数の代わりにFizzBuzzに変換する
+        {
+            private FizzBuzz fizzbuzz;
+
+            // 前準備
+            [SetUp]
+            public void Setup()
+            {
+                fizzbuzz = new FizzBuzz();
+            }
+
+
+            [Test]
+            public void 同値類の中の最小の3と5の公倍数である15を渡すと文字列FizzBuzzを返す()
+            {
+                Assert.AreEqual("FizzBuzz", fizzbuzz.convertTest(15));
+            }
+        }
+
+        class その他の数の時はそのまま文字列に変換する
         {
             private FizzBuzz fizzbuzz;
 
@@ -66,15 +105,38 @@ namespace TddLibraryTest
             }
 
             [Test]
-            public void _1を渡すと文字列1を返す()
+            public void 下限の境界値である1を渡すと文字列1を返す()
             {
                 Assert.AreEqual("1", fizzbuzz.convertTest(1));
             }
 
             [Test]
-            public void _2を渡すと文字列2を返す()
+            public void 下限の境界値の1つ内側である2を渡すと文字列2を返す()
             {
                 Assert.AreEqual("2", fizzbuzz.convertTest(2));
+            }
+        }
+
+        class 仕様の範囲外の値であっても同様の変換規則が適用される
+        {
+            private FizzBuzz fizzbuzz;
+
+            // 前準備
+            [SetUp]
+            public void Setup()
+            {
+                fizzbuzz = new FizzBuzz();
+            }
+
+            [Test]
+            public void 上限の境界値の1つ外側である101を渡すと文字列101を返す()
+            {
+                Assert.AreEqual("101", fizzbuzz.convertTest(101));
+            }
+            [Test]
+            public void 下限の境界値の1つ外側である0は3と5の公倍数でもあるので文字列FizzBuzzを返す()
+            {
+                Assert.AreEqual("FizzBuzz", fizzbuzz.convertTest(0));
             }
         }
     }
